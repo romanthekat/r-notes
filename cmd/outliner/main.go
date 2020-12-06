160b0a42ece3bff07ab8851e6d944186f7d40729
Roman Garanin <git@romangaranin.net>
2020-12-06 15:47:57 +0300

    feat: parse wikilinks and return hierarchy save to file

--- cmd/outliner/main.go
+++ cmd/outliner/main.go
@@ -8,14 +8,33 @@ import (
 	"path/filepath"
 	"regexp"
 	"strings"
+	"time"
 )
 
+const notesDelimiter = "    "
+
+type Note struct {
+	name     string
+	filename string
+
+	parent   *Note
+	children []*Note
+}
+
+func (n Note) String() string {
+	return n.name
+}
+
+func newNote(name string, filename string, parent *Note) *Note {
+	return &Note{name: name, filename: filename, parent: parent}
+}
+
 func main() {
-	filename, folder, err := getFile()
+	file, folder, err := getFile()
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println("generating outline for file", filename)
+	log.Println("generating outline for file", file)
 
 	otherFiles, err := getMdFiles(folder)
 	if err != nil {
@@ -24,12 +43,104 @@ func main() {
 	log.Println("found .md files:", len(otherFiles))
 	log.Println("parsing links")
 
-	content, err := readFile(filename)
+	top := parseNoteHierarchy(file, otherFiles, nil, 3)
+	log.Printf("outline:\n\n")
+
+	hierarchy := getNoteHierarchy(top, "", nil)
+	for _, line := range hierarchy {
+		fmt.Println(line)
+	}
+
+	resultFilename := getResultFilename(file)
+	fmt.Printf("writing to %s\n", resultFilename)
+
+	resultContent := []string{"# " + top.name}
+	resultContent = append(resultContent, hierarchy...)
+	writeToFile(resultFilename, resultContent)
+}
+
+func getResultFilename(file string) string {
+	basePath := filepath.Dir(file)
+	return fmt.Sprintf("%s/%s %s %s.md",
+		basePath,
+		time.Now().Format("200601021504"),
+		"Outline",
+		getNoteName(file),
+	)
+}
+
+func writeToFile(filename string, content []string) {
+	f, err := os.Create(filename)
 	if err != nil {
 		log.Fatal(err)
 	}
-	links := getWikiLinks(content)
-	fmt.Printf("%s\n", links)
+
+	for _, line := range content {
+		_, err := f.WriteString(line + "\n")
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	err = f.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getNoteHierarchy(note *Note, delimiter string, result []string) []string {
+	if note == nil {
+		return result
+	}
+
+	result = append(result, fmt.Sprintf("%s[[%s]]", delimiter, note.String()))
+	for _, child := range note.children {
+		result = getNoteHierarchy(child, delimiter+notesDelimiter, result)
+	}
+
+	return result
+}
+
+func parseNoteHierarchy(file string, files []string, parent *Note, levelsLeft int) *Note {
+	if levelsLeft == 0 {
+		return nil
+	}
+
+	content, err := readFile(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	note := newNote(getNoteName(file), file, parent)
+
+	linkedFiles := getFilesByLinks(file, files, getWikiLinks(content))
+	for _, linkedFile := range linkedFiles {
+		child := parseNoteHierarchy(linkedFile, files, note, levelsLeft-1)
+		if child != nil {
+			note.children = append(note.children, child)
+		}
+	}
+
+	return note
+}
+
+func getNoteName(file string) string {
+	fileName := filepath.Base(file)
+	return strings.TrimSuffix(fileName, filepath.Ext(fileName))
+}
+
+func getFilesByLinks(currentFile string, files []string, wikiLinks []string) []string {
+	var linkedFiles []string
+
+	for _, file := range files {
+		for _, link := range wikiLinks {
+			if file != currentFile && strings.Contains(file, link) {
+				linkedFiles = append(linkedFiles, file)
+			}
+		}
+	}
+
+	return linkedFiles
 }
 
 //getWikiLinks extracts [[LINK] from provided file content
@@ -74,7 +185,7 @@ func getFile() (string, string, error) {
 	}
 
 	filename := os.Args[1]
-	if !strings.HasSuffix(filename, ".md") {
+	if filepath.Ext(filename) == "md" {
 		return "", "", fmt.Errorf("specify .md filename for generating outline")
 	}
 
