fb178cb8bfc6dffa476ea6b73fb9b9b6303b8ea1
Roman Garanin <git@romangaranin.net>
2024-11-28 16:24:06 +0100

    feat: remove w/o, since not needed

--- the-archive-plugins/net.romangaranin.extract_note.thearchiveplugin/main.js
+++ the-archive-plugins/net.romangaranin.extract_note.thearchiveplugin/main.js
@@ -13,26 +13,21 @@ const targetFilename = app.prompt({
 });
 
 
-//temporary workaround, see https://forum.zettelkasten.de/discussion/2996/knownissue-cancel-plugin-execution#latest
-var isError = false
 if (targetFilename === undefined || targetFilename === null || targetFilename.trim() === "") {
-  isError = true
-  //throw new Error("No filename provided by user");
+  cancel("No filename provided by user")
 }
 
-if (!isError) {
-  output.changeFile.filename = targetFilename;
+output.changeFile.filename = targetFilename;
 
-  // Assemble extracted note from a simple template.
-  const targetContent = [
-    `# ${targetFilename}`,
-    "",
-    selectedText,
-    "" // Ensure there's a trailing newline to be a good citizen of plain text files.
-  ].join("\n");
+// Assemble extracted note from a simple template.
+const targetContent = [
+  `# ${targetFilename}`,
+  "",
+  selectedText,
+  "" // Ensure there's a trailing newline to be a good citizen of plain text files.
+].join("\n");
 
-  output.changeFile.content = targetContent;
+output.changeFile.content = targetContent;
 
-  // Replace selection with link to extracted note.
-  output.insert.text = `[[${targetFilename}]]`;
-}
+// Replace selection with link to extracted note.
+output.insert.text = `[[${targetFilename}]]`;
