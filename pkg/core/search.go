957792e76b70927f509e4e6b7465026072e9b8ab
Roman Garanin <git@romangaranin.net>
2025-08-17 17:25:56 +0200

    feat: more relevant notes

--- pkg/core/search.go
+++ pkg/core/search.go
@@ -17,14 +17,14 @@ func GetTopNotes(notes []*Note, level int) []*Note {
 func GetRelevantNotes(mainNote *Note, notes []*Note) []*Note {
 	result := append([]*Note{}, mainNote.Links...)
 
+	result = append(result, mainNote.Links...)
+	result = append(result, mainNote.Backlinks...)
 	//neighbors, f.e. 54a.1 is neighbor/child to 54a
 	for _, note := range notes {
 		if strings.HasPrefix(note.Id, mainNote.Id) {
 			result = append(result, note)
 		}
 	}
-
-	result = append(result, mainNote.Backlinks...)
 
 	return result
 }
